af2e9f16ef9aec152d411b772bc585bb2324fe53
nikblvck <86486046+nikblvck@users.noreply.github.com>
2021-10-18 15:36:53 -0700

    update utils.js, app.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils')
+const db = require('../db/models')
+const { User } = db
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/sign-up', csrfProtection ,asyncHandler(async(req, res) => {
   res.send('respond with a resource');
-});
+
+}));
 
 module.exports = router;
