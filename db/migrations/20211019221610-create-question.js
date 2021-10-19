9c13bd7d80bbb15e0184ed68d8e005b06b0de58b
nikblvck <86486046+nikblvck@users.noreply.github.com>
2021-10-19 15:28:02 -0700

    /questions/ask

---
+++ db/migrations/20211019221610-create-question.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+      return queryInterface.createTable('questions', 'title' {
+        type: 
+       });
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+      return queryInterface.dropTable('questions');
+
+  }
+};
--- routes/questions.js
+++ routes/questions.js
@@ -6,8 +6,8 @@ const { Questions } = db;
 const { check, validationResult } = require('express-validator');
 // TODO: add requiredAuth to access questions text field
 
-router.get('/', asyncHandler(async (req, res) => {
-  res.render('questions')
+router.get('/ask', asyncHandler(async (req, res) => {
+  res.render('ask')
 }));
 
 
