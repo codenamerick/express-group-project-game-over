47f33c79b3a60a299b10647fe029fd642b1039ce
Kendra Chauntell <kendrachauntell@outlook.com>
2021-10-19 18:45:20 -0400

    adding questions route

--- routes/questions.js
+++ routes/questions.js
@@ -6,6 +6,11 @@ const { Questions } = db;
 const { check, validationResult } = require('express-validator');
 // TODO: add requiredAuth to access questions text field
 
+router.get('/', asyncHandler( async (req, res) => {
+  const questions = await Questions.findAll();
+  res.render({ questions });
+}))
+
 router.get('/ask', asyncHandler(async (req, res) => {
   res.render('ask')
 }));
