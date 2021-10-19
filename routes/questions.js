82a79deb4909391b9e232eeea8c57c4a4f3a5c1b
nikblvck <86486046+nikblvck@users.noreply.github.com>
2021-10-19 16:54:12 -0700

    updating questions routes - work in progress

--- routes/questions.js
+++ routes/questions.js
@@ -4,6 +4,8 @@ const { csrfProtection, asyncHandler } = require('./utils');
 const db = require('../db/models');
 const { Question } = db;
 const { check, validationResult } = require('express-validator');
+
+
 // TODO: add requiredAuth to access questions text field
 
 router.get('/', asyncHandler( async (req, res) => {
@@ -15,7 +17,37 @@ router.get('/ask', asyncHandler(async (req, res) => {
   res.render('ask')
 }));
 
+router.get('/:id', asyncHandler(async(req, res) => {
+  const question = await Question.findByPk(id)
 
+  res.render('question-id', { question })
+
+}));
+
+const questionNotFoundError = (id) => {
+  const err = Error("Question not found");
+  err.errors = [`Question with id of ${id} could not be found.`];
+  err.title = "Question not found.";
+  err.status = 404;
+  return err;
+};
+
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const question = await Question.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (question) {
+      res.json({ question });
+    } else {
+      next(questionNotFoundError(req.params.id));
+    }
+  })
+);
 
 
 module.exports = router;
