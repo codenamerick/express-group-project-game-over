ed30d1c317123c7f963531b840b798414791943e
nikblvck <86486046+nikblvck@users.noreply.github.com>
2021-10-19 16:17:46 -0700

    fixed questions route

--- routes/questions.js
+++ routes/questions.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
 const db = require('../db/models');
-const { Questions } = db;
+const { Question } = db;
 const { check, validationResult } = require('express-validator');
 // TODO: add requiredAuth to access questions text field
 
 router.get('/', asyncHandler( async (req, res) => {
-  const questions = await Questions.findAll();
-  res.render({ questions });
+  const questions = await Question.findAll();
+  res.render('questions', { questions } );
 }))
 
 router.get('/ask', asyncHandler(async (req, res) => {
