f0011de643e6ab10a89400e92c3b15948c2f4415
nikblvck <86486046+nikblvck@users.noreply.github.com>
2021-10-20 16:33:16 -0700

    styling update - work in progress

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { Question } = require('../db/models');
+const { asyncHandler, csrfProtection } = require('./utils')
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+// router.get('/', function(req, res, next) {
+//   const questions = Question.findAll({
+//     where:{
 
+//     }
+
+//   })
+//   res.render('index', { questions  });
+// });
+
+router.get('/', csrfProtection, asyncHandler(async(req, res) => {
+
+  const questions = await Question.findAll({
+    limit: 15,
+    order: [["updatedAt", "DESC"]],
+  });
+  res.render('index', { questions })
+}));
 
 module.exports = router;
