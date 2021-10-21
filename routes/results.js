090b0423c15be388a588616d3cf2fbf261c5aaaf
nikblvck <86486046+nikblvck@users.noreply.github.com>
2021-10-21 12:55:02 -0700

    updated search

--- routes/results.js
+++ routes/results.js
@@ -9,16 +9,8 @@ const id = db.User.id
 
 async function searchQuestions(question){
   // let user = User.findById(id)
-   let questions = await Question.findAll({
-     where: {
-       title: {
-         [Op.like]: '`%question%`',
-       },
-     include : {
-      User
-     }},
-   });
-   console.log(questions);
+
+   console.log('test', questions);
    return questions;
  }
 
@@ -29,7 +21,17 @@ router.get(
   csrfProtection,
   asyncHandler(async (req, res) => {
     console.log('we made it into the async function!')
-    let questions = await searchQuestions(`%${req.query.question}`);
+    const { search } = req.query
+     let questions = await Question.findAll({
+       where: {
+         title: {
+           [Op.iLike]: `%${search}%`,
+         },
+       },
+       include: {
+         model: User
+       },
+     });
     console.log(questions);
     console.log('the above listed are question objects')
     res.render("results", { questions });
