76a1bc411509969ff0617232d7c53a522d26940a
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-10-22 10:19:15 -0700

    upvote and downvotes can be created using buttons

--- public/javascripts/votes.js
+++ public/javascripts/votes.js
@@ -15,30 +15,32 @@ window.addEventListener("DOMContentLoaded", (e) => {
 
     // implement down vote btn func below ---
     const downVoteBtns = document.querySelectorAll('.down-vote-btn');
+
     downVoteBtns.forEach(btn => {
         btn.addEventListener('click', async (e) => {
             const answerId = e.target.id.split('-')[2];
 
             const res = await fetch(`/answers/${answerId}/votes`, {
                 method: "POST",
-                headers: {"Content-Type": "application/json"},
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ up_vote: false })
             })
         })
     })
 
     // // implement up vote btn func below ---
-    // const upVoteBtns = document.querySelectorAll('.up-vote-btn');
-    // upVoteBtns.forEach(btn => {
-    //     btn.addEventListener('click', async (e) => {
-    //         const answerId = e.target.id.split('-')[2];
+    const upVoteBtns = document.querySelectorAll('.up-vote-btn');
 
-    //         await fetch(`/answers/${answerId}/votes`, {
-    //             method: "POST",
-    //             header: "application/json",
-    //             body: { "up_vote": "true" }
-    //         })
-    //     })
-    // })
+    upVoteBtns.forEach(btn => {
+        btn.addEventListener('click', async (e) => {
+            const answerId = e.target.id.split('-')[2];
+
+            const res = await fetch(`/answers/${answerId}/votes`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ up_vote: true })
+            })
+        })
+    })
 
 });
